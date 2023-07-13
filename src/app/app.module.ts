3d1bfb73da0778ca1b9e2c893ebe5b4604eba671
cristhian martinez <crifamari@gmail.com>
2023-07-13 18:50:20 -0500

    se eliminan los componentes de saludo  y despedida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { saludo } from './components/saludo.component';
-import { DespedidaComponent } from './components/despedida/despedida.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    saludo,
-    DespedidaComponent
+
   ],
   imports: [
     BrowserModule
