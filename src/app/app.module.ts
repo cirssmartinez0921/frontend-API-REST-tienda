3f55b3e142811bd96a48460df033d4936271d46f
cristhian martinez <crifamari@gmail.com>
2023-07-13 18:54:59 -0500

    se crea el componente login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    LoginComponent,
 
   ],
   imports: [
