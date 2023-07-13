dff593f8a565dfb2d624ef6e7f49a62bd80a20ab
cristhian martinez <crifamari@gmail.com>
2023-07-13 18:55:42 -0500

    se crea el componente register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
+    RegisterComponent,
 
   ],
   imports: [
