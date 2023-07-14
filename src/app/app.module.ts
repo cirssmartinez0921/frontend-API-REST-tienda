0dc5ab2613c1db77b1f06eb804acbfa6afb86fef
cristhian martinez <crifamari@gmail.com>
2023-07-13 19:05:27 -0500

    se crea el componente footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { HeaderComponent } from './shared/header/header.component';
     LoginComponent,
     RegisterComponent,
     HeaderComponent,
+    FooterComponent,
 
   ],
   imports: [
