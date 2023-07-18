85710d780ffeab2cfaaa07ddefaf1dba73034ddc
cristhian martinez <crifamari@gmail.com>
2023-07-17 20:37:03 -0500

    implementamos formularios reactivos para register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  //atributos
+  registerForm: FormGroup = this.fb.group( { 
+    nombre:[
+    '',
+   [
+    Validators.required
+   ]
+  ],
+  
+  email:[
+    '',
+   [
+    Validators.required,
+    Validators.email
+   ]
+  ],
+  password:[
+    '',
+   [
+    Validators.required,
+    Validators.maxLength(8),
+   ]
+]
+});
+ 
+  constructor (private fb: FormBuilder){}
+
+  resgister(){
+
+    console.group("aqui registra al usuario...");
+    console.info(this.registerForm.value);
+    console.info(this.registerForm.valid);
+    console.groupEnd();
+    
+    
+  }
 
 }
