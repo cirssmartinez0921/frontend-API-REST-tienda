44475371b6da3e1606feec004227869004f4276e
cristhian martinez <crifamari@gmail.com>
2023-07-17 20:08:57 -0500

    importamos reactiveformsmodule - implementamos formularios reactivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -7,7 +9,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -21,7 +23,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm: FormGroup = this.fb.group({
+    email: [
+      '', // valor predeterminado de campo
+      [   // validaciones
+        Validators.required,
+        Validators.email
+      ]
+    ],
+    password:[
+      '', // valor predeterminado de campo
+      [   // validaciones
+       Validators.required,
+       Validators.minLength(8)
+      ]
+    ]
+  });
+// inyeccion de dependencias
+ constructor( private fb: FormBuilder ) {};
 
+ // metodo login
+
+ login() {
+   console.group("loginForm");
+   console.info(this.loginForm.value);
+   console.log(this.loginForm.valid);
+   console.groupEnd();
+ }
 }
